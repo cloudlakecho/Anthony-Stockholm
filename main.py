5f09496edb45fbefdb20d7e93ec313256fd87f2d
cloudlakecho <cloudlakecho@yahoo.com>
2018-12-10 23:59:10 -0800

    File split

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
-# main.py in Anthony-Stockholm 
+# main.py in Anthony-Stockholm
 # Cloud Cho May 26, 2018 - Perfomr Pricipal Component Analysis, PCA
 #
 # Error:
 #   error exist
-# 
+#
 # Comment:
 #
 
@@ -23,27 +23,8 @@ from keras.layers import Activation, Dense, Flatten, Conv2D
 
 
 def main():
-    X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-    pca = PCA(n_components=2)
-    pca.fit(X)
-    print(pca.explained_variance_ratio_)  
+    pass
     
-    # Error spot
-    # Cause: version under 0.19
-    #print(pca.singular_values_)  
-
-    pca = PCA(n_components=2, svd_solver='full')
-    pca.fit(X)                 
-    print(pca.explained_variance_ratio_)  
-    #print(pca.singular_values_)  
-
-    pca = PCA(n_components=1, svd_solver='arpack')
-    pca.fit(X)
-    print(pca.explained_variance_ratio_)  
-    #print(pca.singular_values_)  
-
-
 if __name__ == '__main__':
     print(sklearn.__version__)
     main()
-    
