4b62a114e61cb85faad92a4d673dde42e21c7970
cloudlakecho <cloudlakecho@yahoo.com>
2018-12-12 16:36:32 -0800

    Minor update

--- tool/distortion.py
+++ tool/distortion.py
@@ -3,10 +3,10 @@
 # Cloud Cho December 11, 2018 - Calculate distortion of network
 #
 # Error:
-# 
+#
 # Comment:
-# 
-# Reference: 
+#
+# Reference:
 #   https://www.nature.com/articles/srep09450
 #
 
@@ -24,9 +24,15 @@ from keras.models import Sequential
 from keras.layers import Activation, Dense, Flatten, Conv2D
 
 
+def F(X, C):
+    # X(t) = {x_1(t), x_2(t), ...}: population of entities
+    # f_i(x_i): instrinsic dyanmic
+    # F(X) = {f_1(x_1), f_2(x_2)...}: intrinsic dyanamic for whole network
+
 # with other nodes—animal or human population
 # is a prime example. The combination of the evolving dynamics, the
 # peer influence, and the random perturbation leads to a network of
-# stochastic differential equations: dX = (F(X,C) - LX)dt 1 sdW.
+# stochastic differential equations: dX = (F(X,C) - LX)dt + sdW.
 def distort(X, C, W, sigma):
-    pass
+    F(X, C) - LX
+    sigma
