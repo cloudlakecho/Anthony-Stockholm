530557172cb2e7c9dab3851513197001f5bf1ff8
cloudlakecho <cloudlakecho@yahoo.com>
2019-03-30 21:38:40 -0700

    origin master

--- main.py
+++ main.py
@@ -16,15 +16,17 @@ import argparse
 import keras
 import os.path
 
-from pathlib2 import Path #  pipy library
+# from pathlib2 import Path #  pipy library
 from inspect import currentframe, getframeinfo
 from keras.models import Sequential
 from keras.layers import Activation, Dense, Flatten, Conv2D
+from tool import distortion, pca
 
 
 def main():
-    pass
-    
+    pca.pca(X)
+    distortion.F(X, C)
+
 if __name__ == '__main__':
     print(sklearn.__version__)
     main()
